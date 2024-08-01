881715d599ba647c8f608b9c1cc17093b7d327f5
hzt <3061613175@qq.com>
2024-08-02 00:07:25 +0800

    init: install the tailwind and vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const { VuetifyPlugin } = require('webpack-plugin-vuetify')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  configureWebpack: {
+    plugins: [
+      new VuetifyPlugin({
+        // Add your Vuetify options here
+      }),
+    ],
+  },
 })
