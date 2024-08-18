12e47e529042f7bc953acf43ae79921d578d1431
hzt <3061613175@qq.com>
2024-08-18 10:54:18 +0800

    重新认识cols

--- src/components/Post/index.vue
+++ src/components/Post/index.vue
@@ -1,62 +1,82 @@
 <template>
   <v-container fluid>
     <v-row dense>
-      <v-col v-for="c in cards" :key="c.id" cols="12">
-        <v-card :subtitle="c.subtitle" :title="c.title" :text="c.content">
-          <v-card-actions>
+      <v-col v-for="c in cards" :key="c.id" cols="6">
+        <v-card
+          :subtitle="c.subtitle"
+          :title="c.title"
+          :text="c.content"
+          max-width="300"
+        >
+          <v-card-actions >
             <v-row>
               <v-spacer></v-spacer>
 
-              <v-col class="text-center" :cols="1">
+              <v-col class="text-center pa-0 ml-2" :cols="2" >
                 <v-btn density icon @click="like">
                   <v-icon color="red">mdi-heart</v-icon>{{ 1 }}
                 </v-btn>
               </v-col>
 
-              <v-col class="text-center" :cols="1">
+              <v-col class="text-center pa-0 ml-2" :cols="2">
                 <v-btn density icon @click="bookmark">
                   <v-icon>mdi-bookmark</v-icon>{{ 2 }}
                 </v-btn>
               </v-col>
 
-              <v-col class="text-center" :cols="1">
+              <v-col class="text-center pa-0 ml-2" :cols="2">
                 <v-btn density icon @click="share">
                   <v-icon>mdi-share-variant</v-icon>
                 </v-btn>
               </v-col>
 
-              <v-col class="text-center" :cols="1">
+              <v-col class="text-center pa-0 ml-2" :cols="2">
                 <v-btn density icon @click="refresh">
                   <v-icon>mdi-refresh</v-icon>
                 </v-btn>
               </v-col>
             </v-row>
-            <!-- <v-btn color="red" icon="mdi-heart" size="small"></v-btn>
-
-            <div align="center" justify="center">
-              <v-btn
-                density
-                color="medium-emphasis"
-                icon="mdi-heart"
-                size="small"
-              >
-              </v-btn
-              ><br />
-              <span>30</span>
-            </div>
-            <v-btn color="medium-emphasis" icon="mdi-bookmark" size="small">
-            </v-btn>
-            <span>30</span>
-
-            <v-btn
-              color="medium-emphasis"
-              icon="mdi-share-variant"
-              size="small"
-            ></v-btn>
-            <v-btn aria-label="Refresh" icon="mdi-refresh"></v-btn> -->
           </v-card-actions>
         </v-card>
       </v-col>
+      <v-col v-for="c in cards" :key="c.id" cols="6">
+        <v-card
+          :subtitle="c.subtitle"
+          :title="c.title"
+          :text="c.content"
+          width="800"
+        >
+          <v-card-actions>
+            <v-row>
+              <v-spacer></v-spacer>
+
+              <v-col class="text-center" :cols="12">
+                <v-btn density icon @click="like">
+                  <v-icon color="red">mdi-heart</v-icon>{{ 1 }}
+                </v-btn>
+              </v-col>
+
+              <v-col class="text-center" :cols="12">
+                <v-btn density icon @click="bookmark">
+                  <v-icon>mdi-bookmark</v-icon>{{ 2 }}
+                </v-btn>
+              </v-col>
+
+              <v-col class="text-center" :cols="12">
+                <v-btn density icon @click="share">
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-col>
+
+              <v-col class="text-center" :cols="12">
+                <v-btn density icon @click="refresh">
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card></v-col
+      >
     </v-row>
     <v-row>
       <router-view></router-view>
